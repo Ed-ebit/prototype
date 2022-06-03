4f7b0546445f0495f7c79e27ce8bccbf93ed610c
KMV <karl-martin.vogel@tc.de>
2022-06-03 16:29:57 +0200

    Add project files.

---
+++ prototype/Krototype.cs
@@ -0,0 +1,30 @@
+﻿namespace prototype
+{
+    public class Krototype //: IClone<Krototype>
+    {
+        public int CountOfWalls;
+
+        public int CountOfWindows;
+        public string TypeOfRoof;
+        private Krototype child;
+
+
+        public Krototype(int CountOfWindows, int CountOfWalls, string TypeOfRoof, Krototype child)
+        {
+            this.CountOfWindows = CountOfWindows;
+            this.CountOfWalls = CountOfWalls;
+            this.TypeOfRoof = TypeOfRoof;
+            this.child = child;
+        }
+
+        public void PrintHouse()
+        {
+            Console.WriteLine("Fenster: " + this.CountOfWindows + " Wände: " + this.CountOfWalls + " Dach: " + child.TypeOfRoof);
+        }
+
+        public Krototype Clone()
+        {
+            return new Krototype(this.CountOfWindows, this.CountOfWalls, this.TypeOfRoof, this.child?.Clone());
+        }
+    }
+}
---
+++ prototype/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace prototype
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            Krototype child = new Krototype(0,0, "SuperCooles Roof", null);
+            Krototype Haus1 = new Krototype(3, 4, "Spitzdach",child);
+            Haus1.PrintHouse();
+            var Haus2 = Haus1.Clone();
+
+            child.TypeOfRoof = "neues Roof";
+            Haus2.CountOfWalls = 1;
+            Haus2.PrintHouse();
+        }
+    }
+}
